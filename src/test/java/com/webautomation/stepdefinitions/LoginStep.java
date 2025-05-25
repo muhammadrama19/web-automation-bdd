587125a79bb932152809cf6f95e78813f3b53665
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2025-05-25 17:09:31 +0700

    [Refactor] New TC for invalid credentials also change some of the step using parameterized instead

--- src/test/java/com/webautomation/stepdefinitions/LoginStep.java
+++ src/test/java/com/webautomation/stepdefinitions/LoginStep.java
@@ -27,7 +27,7 @@ public class LoginStep {
 
     @When("user input username {string} and password {string}")
     public void user_input_valid_credentials(String username, String password) {
-        loginAction.loginStep("bendahara", "admin123");
+        loginAction.loginStep(username, password);
     }
 
     @And("user click login button")
@@ -43,7 +43,7 @@ public class LoginStep {
 
     @When("user input wrong username {string} and password {string}")
     public void user_input_invalid_credentials_username(String username, String password) {
-        loginAction.loginStep("indra", "admin123");
+        loginAction.loginStep(username, password);
     }
 
     @Then("user should see the error message for incorrect username {string}")
@@ -53,7 +53,7 @@ public class LoginStep {
 
     @When("user input username {string} and wrong password {string}")
     public void user_input_invalid_credentials_password(String username, String password) {
-        loginAction.loginStep("bendahara", "admin1234");
+        loginAction.loginStep(username, password);
     }
 
     @Then("user should see the error message for incorrect password {string}")
@@ -77,4 +77,19 @@ public class LoginStep {
         loginAction.setField(fieldName, value);
     }
 
+    @When("user input wrong formatted username {string}")
+    public void user_input_wrong_formatted_username(String username) {
+        loginAction.setField("Username", username);
+    }
+
+    @And ("user input password {string}")
+    public void user_input_password(String password) {
+        loginAction.setField("Password", password);
+    }
+
+    @Then("user should be able to see message for wrong formatted username {string}")
+    public void user_should_be_able_to_see_message_for_wrong_formatted_username(String s) {
+        boolean isMessageDisplayed = loginAction.isTextExists("Incorrect username or password, please try again!");
+        assertTrue(isMessageDisplayed);
+    }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -6,19 +6,19 @@ Feature: Login Zaidan Educare
     And user accessing url http://ptbsp.ddns.net:6882/
     And user is not logged in
 
-  @ValidCredentials @TC2.1
+  @Positive @TC2.1
   Scenario: Login with valid credentials
     When user input username "bendahara" and password "admin123"
     And user click login button
     Then user should see the dashboard page
 
-  @InvalidCredentials @TC2.3
+  @Negative @TC2.3
   Scenario: Login with invalid credentials username
     When user input wrong username "indra" and password "admin123"
     And user click login button
     Then user should see the error message for incorrect username "Incorrect username or password, please try again!"
 
-  @InvalidCredentials @TC2.4
+  @Negative @TC2.4
   Scenario: Login with invalid credentials password
     When user input username "bendahara" and wrong password "admin1234"
     And user click login button
@@ -47,3 +47,10 @@ Feature: Login Zaidan Educare
       And user click login button
       Then user should be able to see message "Username wajib diisi" under username input field
       And user should be able to see message "Password wajib diisi" under password input field
+    
+    @Negative @TC2.8
+    Scenario: Login with wrong formatted username
+    When user input wrong formatted username "BENDAHARA"
+    And user input password "admin123"
+    And user click login button
+    Then user should be able to see message for wrong formatted username "Incorrect username or password, please try again!"
