79a3441d1032ad4856ee0252311ff890943d0c66
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2025-05-25 10:42:57 +0700

    [Refactor] Update DashboardLocator and DashboardAction to use WebElement for dashboard title and profile checks

--- src/test/java/com/webautomation/automation/pagelocator/DashboardLocator.java
+++ src/test/java/com/webautomation/automation/pagelocator/DashboardLocator.java
@@ -1,13 +1,14 @@
 package com.webautomation.automation.pagelocator;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class DashboardLocator {
 
     @FindBy(xpath ="/html/body/div[2]/main/div/div[1]/div/div/div[1]/h1")
-    public String bendaharaDashboardTitle;
+    public WebElement bendaharaDashboardTitle;
 
     @FindBy(xpath ="/html/body/div[2]/main/div/div[1]/div/div/div[2]/span")
-    public String bendaharaProfile;
+    public WebElement bendaharaProfile;
 
 }
