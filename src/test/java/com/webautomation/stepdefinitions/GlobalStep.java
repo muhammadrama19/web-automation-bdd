dd7a83782d4b28390614427744911219c6f86f9f
aryrk <amanwhohave@gmail.com>
2025-05-27 10:22:39 +0700

    Add Allure results for negative login scenarios
    - Created result JSON for "Login with empty username and password" scenario.
    - Created container JSON for the scenario "Login with empty username".
    - Added result JSON for "Login with empty username" scenario.
    - Added result JSON for "Login with invalid credentials username" scenario.

--- src/test/java/com/webautomation/stepdefinitions/GlobalStep.java
+++ src/test/java/com/webautomation/stepdefinitions/GlobalStep.java
@@ -1,5 +1,11 @@
 package com.webautomation.stepdefinitions;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import java.io.File;
+
 import com.webautomation.automation.action.LoginAction;
 import com.webautomation.automation.utils.SeleniumHelper;
 
@@ -7,6 +13,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
 
 public class GlobalStep {
@@ -35,5 +42,19 @@ public class GlobalStep {
     @AfterAll
     public static void after_all() {
         SeleniumHelper.closeDriver();
+    }
+
+     @After
+    public void tearDown(Scenario scenario) {
+        WebDriver driver = SeleniumHelper.getDriver();
+        if (scenario.isFailed() && driver != null) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Failed_Screenshot");
+            try {
+                FileUtils.copyFile(((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE), new File("screenshots/" + scenario.getName() + ".png"));
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
